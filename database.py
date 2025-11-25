855bcd90167a27491c83c207cb6abcfb4b8ece0d
pro <stephanedjatio@gmail.com>
2025-11-25 05:54:30 +0100

    Fix .gitignore not working

--- database.py
+++ database.py
@@ -9,15 +9,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", "3306")
 DB_NAME = os.getenv("DB_NAME")
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/db_president_work"
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@127.0.0.1:3306/almapps_new_db"
-# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://kaiseioy_almapps_user:B)h#W?Xs3}JP@162.0.209.239:3306/kaiseioy_almapps_new"
-# SQLALCHEMY_DATABASE_URL = (
-#     "mysql+pymysql://almapps_user:AlmappsConnect2025!@127.0.0.1/almapps_db"
-# )
-# SQLALCHEMY_DATABASE_URL = (
-#     f"mysql+pymysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-# )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
